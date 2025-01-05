428ab4a51e1d8dd836bd61af27598c4eee2a135f
cgtyklsn09 <cgtyklsn@adresiniz.com>
2025-01-05 18:05:30 +0300

    IdentityApp created.

---
+++ Models/IdentityContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public class IdentityContext : IdentityDbContext<IdentityUser>
+    {
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("Data Source=databse.dat");
+        //}
+
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+            
+        }
+    }
+}
